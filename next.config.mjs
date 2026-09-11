c7aa5ea66487400125366eb766253cc732adc248
khoshgoftar5-png <khoshgoftar5@gmail.com>
2026-09-11 18:39:39 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+output: 'export',
   reactStrictMode: true,
   images: {
+unoptimized:tru,
     remotePatterns: [{ protocol: "https", hostname: "**" }]
   }
 };
