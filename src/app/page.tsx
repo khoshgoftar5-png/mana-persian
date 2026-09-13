51643c1476fc3c470eba3352e71855ad62a8da0a
khoshgoftar5-png <khoshgoftar5@gmail.com>
2026-09-13 14:28:47 +0330

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function RootRedirect() {
+  return (
+    <div style={{ padding: "3rem", textAlign: "center" }}>
+      <p>
+        در حال انتقال به نسخهٔ فارسی… اگر منتقل نشدید، <a href="/fa/" style={{ color: "#D8B876" }}>اینجا</a> را بزنید.
+      </p>
+    </div>
+  );
+}
