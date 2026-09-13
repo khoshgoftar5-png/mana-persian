9bfc1c2134ec978d8ab7294301c0eba7080a3b11
khoshgoftar5-png <khoshgoftar5@gmail.com>
2026-09-13 18:47:56 +0330

    Delete src/app/page.tsx

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootRedirect() {
-  return (
-    <div>
-      <p>
-        در حال انتقال به نسخه فارسی... اگر منتقل نشدید <a href="/fa/">اینجا</a> را بزنید.
-      </p>
-    </div>
-  );
-}
