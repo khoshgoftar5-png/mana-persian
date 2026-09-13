21bcd1437ea49e6ddc5c8033d306d002a4da63e1
khoshgoftar5-png <khoshgoftar5@gmail.com>
2026-09-13 14:59:30 +0330

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-export default function RootLayout({ children}:{children:react.reactnode}) {
+export default function RootLayout({ children}:{children:React.ReactNode}) {
   return (
     <html lang="fa" dir="rtl">
       <head>
