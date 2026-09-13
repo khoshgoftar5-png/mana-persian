5bff2a15b056ed79ddd50eeecb3338c658fd2990
khoshgoftar5-png <khoshgoftar5@gmail.com>
2026-09-13 14:51:36 +0330

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-Export default function RootLayout({ children }) {
-  Return (
+export default function RootLayout({ children }) {
+  return (
     <html lang="fa" dir="rtl">
       <head>
         <meta httpEquiv="refresh" content="0; url=/fa/" />
       </head>
       <body>{children}</body>
-      </html>
+    </html>
   );
 }
